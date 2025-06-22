109ffecac79806f3f9973515538b1e9283e347fc
cgpranavi <pothana-pranavi.naidu@capgemini.com>
2025-06-22 08:51:11 +0000

    agent added

--- ticketagent/agent.py
+++ ticketagent/agent.py
@@ -1,20 +1,16 @@
-import datetime
-from zoneinfo import ZoneInfo
 from google.adk.agents import Agent
+
 
 def get_ticket_data(ticket_id: str) -> dict:
     """Retrieves ticket data from a hypothetical GCP data source.
-
     Args:
         ticket_id (str): The ID of the ticket to retrieve.
-
     Returns:
         dict: status and result or error message.
     """
     # In a real application, this would involve querying a GCP data source
     # such as BigQuery or Cloud SQL.  Here, we simulate the behavior with
     # hardcoded data for ticket ID "12345" and an error for other IDs.
-
     if ticket_id == "12345":
         return {
             "status": "success",
@@ -30,16 +26,10 @@ def get_ticket_data(ticket_id: str) -> dict:
     else:
         return {"status": "error", "error_message": f"Ticket '{ticket_id}' not found."}
 
-
 root_agent = Agent(
     name="ticket_resolution_agent",
     model="gemini-2.0-flash",
-    description=(
-        "Agent to answer questions about the given ticket."
-    ),
-    instruction=(
-        "You are a helpful agent who can answer user questions about the ticket."
-    ),
-    tools=[get_ticket_data],
-    #tools=[Tool(function=get_ticket_data)],
+    description="Agent to answer questions about the time and weather in a city.",
+    instruction="I can answer your questions about the time and weather in a city.",
+    tools=[get_ticket_data]
 )
